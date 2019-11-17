7855fee2923d4b576b663cdc5639c09c6265f68f
Matthew J. Hughes <mjhughes707@gmail.com>
2019-11-17 14:51:32 -0800

    removed unnecessary folders / began basic components

---
+++ src/components/MemeGenerator.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+class MemeGenerator extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      data: null
+    }
+  }
+
+  render() {
+    return (
+      <p>Meme Content</p>
+    )
+  }
+}
+
+
+export default MemeGenerator
