47528eb26fafa3d21a87a8a78968d8657d7e628e
Matthew J. Hughes <mjhughes707@gmail.com>
2019-11-18 13:30:44 -0800

    updated header image as a static file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from "react"
 
+import Cartoon from "../img/black-cat-cartoon.png"
+
 function Header() {
   return (
     <header>
       <img 
-        src="https://i.dlpng.com/static/png/5228797-download-cartoon-cats-png-animated-black-cat-gif-png-image-with-cartoon-cat-png-423_362_preview.png"
+        src={Cartoon}
         alt="Black Cat Cartoon"
       />
       <h1>Make-A-Meowm</h1>
